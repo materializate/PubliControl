69018b10c6835bb9cb908acf135e040259bb13bc
materializate <materializate@gmail.com>
2026-04-09 09:29:16 +0200

    Update Supabase configuration with new URL and key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-window.SUPABASE_URL  = '';
-window.SUPABASE_ANON = '';
+window.SUPABASE_URL  = 'https://uawizeotympvtebertbw.supabase.co';
+window.SUPABASE_ANON = 'sb_publishable_uDurLakIB-SH6qKEtNJnVQ_4gzBoO2x';
 window.EPG_PROXY_URL = window.SUPABASE_URL ? window.SUPABASE_URL + '/functions/v1/epg-proxy' : '';
